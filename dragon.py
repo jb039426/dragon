791a33b66dfa0ba5c894f6fcb29e80179d1eff57
jb039426 <125678724+jb039426@users.noreply.github.com>
2023-02-17 08:34:15 -0700

    Create dragon.py

---
+++ dragon.py
@@ -0,0 +1,48 @@
+import turtle
+t = turtle.Turtle()
+s = turtle.Screen()
+
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(110)
+
+t.right(90)
+t.forward(80)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(20)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(190)
+
+t.right(90)
+t.forward(70)
+t.right(90)
+t.forward(20)
+t.right(90)
+t.forward(70)
+
+while True<2:
+    t.left(90)
+    t.forward(30)
+    t.left(90)
+    t.forward(70)
+    t.right(90)
+    t.forward(20)
+    t.right(90)
+    t.forward(70)
+
+
+
+s.exitonclick()
